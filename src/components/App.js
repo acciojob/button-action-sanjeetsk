34d72cd1c6d93eb353d09c16188a089123000b46
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-05 10:24:17 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,20 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  const[isVisible, setIsVissible] = useState(false);
+
+  function handleClick(){
+    setIsVissible(true);
+  }
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>ClickMe</button>
+      <p id="para" className={isVisible ? 'show' : 'hide'}>
+      Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
     </div>
   );
 }
 
-export default App
+export default App;
